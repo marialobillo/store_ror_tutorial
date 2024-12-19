3b0c93815668cd59c60a41cfa69a97f312111158
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-12-19 17:56:36 +0100

    fix: all routes at once for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/products", to: "products#index"
+  
+  # get "/products", to: "products#index"
+
+  # get "/products/new", to: "products#new"
+  # post "/products", to: "product#create"
+
+  # get "/products/:id", to: "products#show"
+
+  # get "/products/:id/edit", to: "products#edit"
+  # patch "/products/:id", to: "products#update"
+  # put "/products/:id", to: "products#update"
+
+  # delete "/products/:id", to "products#destroy"
+  resources :products
+  
 end
