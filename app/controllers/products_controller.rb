090c9c845f4cc07cb4bfdd2541a14c77610366b1
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-12-20 10:25:06 +0100

    fix: write update correctly

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[ show edit udpate destroy ]
+  before_action :set_product, only: [ :show, :edit, :update, :destroy ]
   
   def index
     @products = Product.all
